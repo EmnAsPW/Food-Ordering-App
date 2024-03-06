f19fc4c76e089da5c8bfdce2999fdc0cb01ee9c6
EmnAsPW <emonsourov@gmail.com>
2024-03-06 17:06:24 +0600

    registration done & will work on login

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,3 +1,4 @@
+//import bcrypt from "bcrypt";
 import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema(
@@ -9,12 +10,23 @@ const UserSchema = new Schema(
       validate: (pass) => {
         if (!pass?.length || pass.length < 5) {
           new Error("Password must be at least 5 character");
-          return false;
+          //return false;
         }
       },
     },
   },
   { timestamps: true }
 );
+
+// UserSchema.pre("save", (next, ...rest) => {
+//   console.log(rest);
+//   next();
+// });
+
+// UserSchema.post("validate", function (user) {
+//   const notHashedPassword = user.password;
+//   const salt = bcrypt.genSaltSync(10);
+//   user.password = bcrypt.hashSync(notHashedPassword, salt);
+// });
 
 export const User = models?.User || model("User", UserSchema);
